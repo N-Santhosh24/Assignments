78052f71f1ca8da59c272aabaeb64cb2776afcee
N-Santhosh24 <gowdasanthosh213@gmail.com>
2025-11-15 22:39:55 +0530

    Initial Commit

---
+++ Assignment1.py
@@ -0,0 +1,15 @@
+
+a = int(input("Enter a number: "))
+b = int(input("Enter another number: "))
+Addition = a + b
+Subtraction = a - b
+Multiplication = a * b
+Division = a / b
+print("Addition:", Addition)
+print("Subtraction:", Subtraction)
+print("Multiplication:", Multiplication)
+print("Division:", Division)
+c= input("Enter your first name:")
+d= input("Enter your last name:")
+print("Hello,",c,d+"! Welcome to Python program")
+
